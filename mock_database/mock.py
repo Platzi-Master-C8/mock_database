e382fc387efc8c0487161191646e92e4e90343dd
Fernando Jerez <fernandojerez@gmail.com>
2022-01-11 12:30:03 -0500

    remove industry and source

--- mock_database/mock.py
+++ mock_database/mock.py
@@ -1,6 +1,6 @@
 from mock_database.generators import enum, value, model, phrase, company_name, text, sequence, random_int, name, \
     random_float, url
-from mock_database.model import Company, Industry, Source, Skill
+from mock_database.model import Company, Skill
 
 position_title = phrase(
     enum(['Senior', "Junior"]),
@@ -8,7 +8,6 @@ position_title = phrase(
     value("Developer")
 )
 
-industries = ["E-commerce", "Fintech", "Search Engines", "Gaming"]
 skills = ["Python", "Java", "Kotlin", "React", "Angular", "Go", "TensorFlow", "Spring"]
 companies = 50
 
@@ -16,21 +15,9 @@ Skills = model(Skill, Skill.id_skill, {
     Skill.skill: sequence(skills)
 }, len(skills))
 
-Industries = model(Industry, Industry.id_industry, {
-    Industry.industry: sequence(industries),
-    Industry.description: text(150)
-}, len(industries))
-
-Sources = model(Source, Source.id_source, {
-    Source.description: text(500),
-    Source.url: url(),
-    Source.documentation: text(500)
-}, companies)
-
 Companies = model(Company, Company.id_company, {
     Company.name: company_name(50),
     Company.description: text(150),
-    Company.industry_id: Industries.id(),
     Company.company_size: random_int(10, 3000),
     Company.ceo: name(50),
     Company.avg_reputation: random_float(0, 100),
@@ -40,12 +27,9 @@ Companies = model(Company, Company.id_company, {
     Company.culture_score: random_float(0, 100),
     Company.work_life_balance: random_float(0, 100),
     Company.stress_level: random_float(0, 100),
-    Company.source_id: Sources.id()
 }, companies)
 
 
 def mock_data():
     Skills()
-    Industries()
-    Sources()
     Companies()
